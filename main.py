b848b16e7980ba7f5fc49befea2d650f150a4a99
Rostislav <71731666+zrafoo@users.noreply.github.com>
2024-08-20 18:11:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from aiogram.types import FSInputFile
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters.command import Command
 
+text_1=""
+text_2=""
+
 kb_start = [
         [types.KeyboardButton(text="Информация"),types.KeyboardButton(text="Обменники")],
         [types.KeyboardButton(text="Личный кабинет"),types.KeyboardButton(text="Избранные обменники")],
@@ -21,12 +24,7 @@ dp = Dispatcher()
 
 @dp.message(Command("start"))
 async def process_start_command(message: types.Message):
-    await message.answer_photo(start_image, caption='''
-                            Мониторинг обменников <strong>BestChange</strong>
-                            
-Добро пожаловать на телеграм бота мониторинга обменников <strong>BestChange</strong>! 
-Наш мониторинг обменных пунктов разработан для тех, кто хочет безопасно обменивать электронные деньги в сети интернет с незначительными комиссионными затратами.
-                                                    ''', reply_markup=keyboard_start)
+    await message.answer_photo(start_image, caption=text_1, reply_markup=keyboard_start)
 
 @dp.message(lambda message: message.text == "Обменники")
 async def exchangers_list(message: types.Message):
@@ -40,11 +38,7 @@ async def process_start_command(message: types.Message):
 
 @dp.message(lambda message: message.text == "Информация")
 async def exchangers_list(message: types.Message):
-    await message.answer_photo(task_image,caption='''
-    <strong>Подробнее о мониторинге обменников</strong>
-
-В интернете существует множество обменных пунктов электронных валют, и каждый обменник устанавливает свои курсы, постоянно их корректируя. При этом даже самые авторитетные и крупные обменные пункты не всегда могут иметь в наличии необходимую вам сумму. Как же найти самый оптимальный обменник, где можно беспрепятственно обменять деньги с минимальными потерями? Использование сервиса по мониторингу обменных пунктов поможет вам сэкономить время и деньги. Поиск курсов ведется по нескольким десяткам проверенных и надежных автоматических электронных обменников.
-    ''', reply_markup=keyboard_start)
+    await message.answer_photo(task_image,caption=text_2, reply_markup=keyboard_start)
 
 
 
